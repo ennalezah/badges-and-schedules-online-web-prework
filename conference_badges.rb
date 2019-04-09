299f429fde3da0090a08bf82c40339749ed7c6e1
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-09 07:13:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_arr)
+  badge_messages = []
+  
+  names_arr.each do |name|
+    badge_messages.push(badge_maker(name))
+  end
+  
+  badge_messages
+end
+
+def assign_rooms
+end
+
+def printer
+end 
