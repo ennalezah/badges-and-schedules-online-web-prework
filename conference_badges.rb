b9b261e9333b304cb0cf09430f3a1e36b6c6b718
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-09 07:42:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,14 @@ def batch_badge_creator(names_arr)
   badge_messages
 end
 
-def assign_rooms(names_arr)
-  "Hello #{name}! You'll be assigned to room #{room}"
+def assign_rooms(names_arr
+  
+  
+  names_arr.each do |name|
+    room = 1 
+    
+    "Hello #{name}! You'll be assigned to room #{room}!"
+  end
 end
 
 def printer
