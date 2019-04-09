63c87fbd530038f8ebb7db4bcbef75bfe8d6e692
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-09 07:57:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,15 +13,15 @@ def batch_badge_creator(names_arr)
 end
 
 def assign_rooms(names_arr)
-  room_assignment = []
-  room_count = 0
+  # room_assignment = []
+  # room_count = 0
   
-  names_arr.each_with_index { |val,room+1| puts "index: #{index} for #{val}" if val <= 7}
+  names_arr.each_with_index { |name, (room+1)| puts "Hello, #{name}! You'll be assigned to room #{room}!" if room <= 7}
   
-  names_arr.each do |name|
-    room_assignment.push("Hello, #{name}! You'll be assigned to room #{room_count}!")
-    room_count += 1
-  end
+  # names_arr.each do |name|
+  #   room_assignment.push("Hello, #{name}! You'll be assigned to room #{room_count}!")
+  #   room_count += 1
+  # end
   
   room_assignment
 end
